1802f87cd112dd840f9585017caf0b5986fd276e
pranay <pranaykawale500@gmail.com>
2021-12-08 13:39:53 +0530

    Added:Calculate fare for cab

---
+++ src/test/java/com/bridgelabz/CabInvoiceGenerator/CabInvoiceTest.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.CabInvoiceGenerator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class CabInvoiceTest 
+{
+	CabInvoice cabInvoice=new CabInvoice();
+	@Test
+	public void ifExpectedTotalFareIsGreaterThanMinimumFare_shouldPass(){
+		double totalFare= cabInvoice.calculateFare(5.0, 2);
+		Assert.assertEquals(52.0,totalFare,0);
+
+	}
+	@Test
+	public void ifExpectedTotalFareIsLesserThanMinimumFare_shouldPass(){
+		double totalFare= cabInvoice.calculateFare(0, 2);
+		Assert.assertEquals(5,totalFare,0);
+
+	}
+}
