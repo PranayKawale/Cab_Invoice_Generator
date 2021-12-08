353dcd8b7ae50a7fff4a1762d7f1e3045136dc42
pranay <pranaykawale500@gmail.com>
2021-12-08 13:28:42 +0530

    Added:calculate fare for cab

--- src/main/java/com/bridgelabz/CabInvoiceGenerator/CabInvoice.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator/CabInvoice.java
@@ -3,8 +3,20 @@ package com.bridgelabz.CabInvoiceGenerator;
 
 public class CabInvoice 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to the Cab Invoice Generator Problem" );
-    }
+	private static final int COST_PER_KM=10;
+	private static final int COST_PER_MINUTE=1;
+	private static final double MINIMUM_FARE=5.0;
+
+	double calculateFare(double distance, int time){
+		double totalFare=((distance*COST_PER_KM)+(time*COST_PER_MINUTE));
+		if (totalFare>MINIMUM_FARE)
+			return totalFare;
+		else {
+			return MINIMUM_FARE;
+		}
+	}
+	public static void main( String[] args )
+	{
+		System.out.println( "Welcome to the Cab Invoice Generator Problem" );
+	}
 }
