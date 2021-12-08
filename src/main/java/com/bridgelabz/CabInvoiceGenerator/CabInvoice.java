964ab99c9b6d747f2fe06701723e52add6a12c35
pranay <pranaykawale500@gmail.com>
2021-12-08 16:19:52 +0530

    Added:Generating summary of rides and fare

--- src/main/java/com/bridgelabz/CabInvoiceGenerator/CabInvoice.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator/CabInvoice.java
@@ -2,29 +2,32 @@ package com.bridgelabz.CabInvoiceGenerator;
 
 
 public class CabInvoice 
-{
+{ 
 	private static final int COST_PER_KM=10;
 	private static final int COST_PER_MINUTE=1;
 	private static final double MINIMUM_FARE=5.0;
 
-	public double calculateFare(double distance, int time){
+	double calculateFare(double distance, int time){
 		double totalFare=((distance*COST_PER_KM)+(time*COST_PER_MINUTE));
-		if (totalFare>MINIMUM_FARE)
-			return totalFare;
-		else {
-			return MINIMUM_FARE;
-		}
+		return Math.max(totalFare, MINIMUM_FARE);
 	}
-	public static void main( String[] args )
-	{
-		System.out.println( "Welcome to the Cab Invoice Generator Problem" );
+	public static void main(String[] args) {
+		System.out.println("Welcome to cab invoice generator problem");
 	}
 
-    public double calculateFare(Ride[] rides) {
-        double totalFare=0;
-        for (Ride ride:rides){
-            totalFare=totalFare+this.calculateFare(ride.distance,ride.time);
-        }
-        return totalFare;
-    }
+	public double calculateTotalFare(Ride[] rides) {
+		double totalFare=0;
+		for (Ride ride:rides){
+			totalFare=totalFare+this.calculateFare(ride.distance,ride.time);
+		}
+		return totalFare;
+	}
+
+	public InvoiceSummary calculateFare(Ride[] rides) {
+		double totalFare=0;
+		for (Ride ride:rides){
+			totalFare=totalFare+this.calculateFare(ride.distance,ride.time);
+		}
+		return new InvoiceSummary(rides.length, totalFare);
+	}
 }
--- src/test/java/com/bridgelabz/CabInvoiceGenerator/CabsInvoiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGenerator/CabsInvoiceTest.java
@@ -1,26 +1,45 @@
 package com.bridgelabz.CabInvoiceGenerator;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CabsInvoiceTest {
-	  CabInvoice cabInvoice=new CabInvoice();
-	    @Test
-	    public void ifExpectedTotalFareIsGreaterThanMinimumFare_shouldPass(){
-	        double totalFare= cabInvoice.calculateFare(5.0, 2);
-	        Assert.assertEquals(52.0,totalFare,0);
+	 CabInvoice cabInvoice = new CabInvoice();
+
+	    @Before
+	    public void setUp() throws Exception {
+
 	    }
+
 	    @Test
-	    public void ifExpectedTotalFareIsLesserThanMinimumFare_shouldPass(){
-	        double totalFare= cabInvoice.calculateFare(0, 2);
-	        Assert.assertEquals(5,totalFare,0.0);
+	    public void ifExpectedTotalFareIsGreaterThanMinimumFare_shouldPass() {
+	        double totalFare = cabInvoice.calculateFare(5.0, 2);
+	        Assert.assertEquals(52.0, totalFare, 0);
+	    }
+
+	    @Test
+	    public void ifExpectedTotalFareIsLesserThanMinimumFare_shouldPass() {
+	        double totalFare = cabInvoice.calculateFare(0, 2);
+	        Assert.assertEquals(5, totalFare, 0.0);
 	    }
 	    @Test
 	    public void giveMultipleRide_shouldReturnTotalFare() {
 	        Ride[] rides = {new Ride(2.0, 5),
 	                new Ride(0.1, 1)
 	        };
-	        double fare=cabInvoice.calculateFare(rides);
+	        double fare = cabInvoice.calculateTotalFare(rides);
 	        Assert.assertEquals(30, fare, 0);
+	    }
+
+
+	    @Test
+	    public void giveMultipleRide_shouldReturnInvoiceSummary() {
+	        Ride[] rides = {new Ride(2.0, 5),
+	                new Ride(0.1, 1)
+	        };
+	        InvoiceSummary summary = cabInvoice.calculateFare(rides);
+	        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30);
+	        Assert.assertEquals(expectedInvoiceSummary, summary);
 	    }
 	}
